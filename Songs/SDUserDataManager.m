e5e60106148ffe34e1194506874d01b524d20192
Steven Degutis <steven@cleancoders.com>
2013-08-20 23:44:13 -0500

    faster

--- Songs/SDUserDataManager.m
+++ Songs/SDUserDataManager.m
@@ -148,22 +148,8 @@
 }
 
 + (NSArray*) songsForUUIDs:(NSArray*)songUUIDs {
-    NSMutableArray* foundSongs = [NSMutableArray array];
-    
     NSArray* allSongs = [[SDUserDataManager sharedMusicManager] allSongs];
-    
-    for (NSString* uuid in songUUIDs) {
-        NSUInteger songIndex = [allSongs indexOfObjectPassingTest:^BOOL(SDSong* otherSong, NSUInteger idx, BOOL *stop) {
-            return [[otherSong valueForKey:@"uuid"] isEqualToString:uuid];
-        }];
-        
-        if (songIndex != NSNotFound) {
-            SDSong* song = [allSongs objectAtIndex:songIndex];
-            [foundSongs addObject:song];
-        }
-    }
-    
-    return foundSongs;
+    return [allSongs filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"uuid in %@", songUUIDs]];
 }
 
 
