fec9a9cf5fb5cea91312471935bee62a9a7ea43e
Steven Degutis <steven@cleancoders.com>
2013-08-25 15:56:29 -0500

    alternative volume slider ui

--- Songs/SDVolumeSliderCell.m
+++ Songs/SDVolumeSliderCell.m
@@ -13,36 +13,24 @@
 @implementation SDVolumeSliderCell
 
 - (void)drawBarInside:(NSRect)aRect flipped:(BOOL)flipped {
-    
-    [NSGraphicsContext saveGraphicsState];
-    
     aRect = NSInsetRect(aRect, 4.0, 0.0);
     
     CGFloat r = 2.0;
     NSBezierPath* wholePath = [NSBezierPath bezierPathWithRoundedRect:aRect xRadius:r yRadius:r];
     
-    [wholePath addClip];
-    
     [SDVolumeSliderBackColor setFill];
     [wholePath fill];
-    
-    
-    
-    
-    CGFloat h = NSHeight(aRect);
-    CGFloat y = h * ([self doubleValue]);
-    
-    NSRect knobRect = aRect;
-    knobRect.origin.y = h - y;
-    knobRect.size.height = h;
-    
-    [SDVolumeSliderForeColor setFill];
-    [[NSBezierPath bezierPathWithRoundedRect:knobRect xRadius:r yRadius:r] fill];
-    
-    [NSGraphicsContext restoreGraphicsState];
 }
 
 - (void)drawKnob:(NSRect)knobRect {
+    knobRect = NSInsetRect(knobRect, 4.0, 0.0);
+    knobRect = NSInsetRect(knobRect, 2.0, 2.0);
+    
+    CGFloat r = 1.0;
+    NSBezierPath* knobPath = [NSBezierPath bezierPathWithRoundedRect:knobRect xRadius:r yRadius:r];
+    
+    [SDVolumeSliderForeColor setFill];
+    [knobPath fill];
 }
 
 //- (NSRect)knobRectFlipped:(BOOL)flipped {
